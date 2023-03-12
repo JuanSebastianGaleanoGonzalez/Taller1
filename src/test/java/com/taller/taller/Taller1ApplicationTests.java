98d94683954167a09329cabe2a56fe73e9b68765
JuanSebastianGaleanoGonzalez <71532153+JuanSebastianGaleanoGonzalez@users.noreply.github.com>
2023-03-11 22:47:39 -0500

    Commit Inicial

---
+++ src/test/java/com/taller/taller/Taller1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller.taller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
